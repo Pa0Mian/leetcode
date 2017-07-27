48cca423c58c9147b95d7c81842c077938576cfa
吴志欣 <wzx@meitu.com>
2017-07-27 20:21:35 +0800

    【Pro5】 第五题最长回文初步提交

--- src/leetcode/Solution05.java
+++ src/leetcode/Solution05.java
@@ -1,33 +1,93 @@
 package leetcode;
 
-import leetcode.util.Stack;
+
+import java.util.LinkedList;
 
 /**
  * Created by wzx on 2017/7/26.
  */
 public class Solution05 {
-    public static String longestPalindrome(String s) {
-        Stack<Character> stack = new Stack<>();
+    // 中心扩展法：0(n^3)
+    public static String longestPalindrome1(String s) {
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
+        if (s.length() == 1) {
+            return s;
+        }
         int i = 0;
-        int j = 0;
-        while ( i + 1 < s.length()) {
-            if (!stack.isEmpty()) {
-                if (stack.peek() == s.charAt(i)) {
-                    System.out.print(stack.peek());
-                    i++;
-                    break;
+        LinkedList<Character> palindromeMax = new LinkedList<>();
+        while (i < s.length()) {
+            LinkedList<Character> palindrome = new LinkedList<>();
+            if (i + 1 < s.length()) {
+                if (s.charAt(i+1) == s.charAt(i)) {
+                    int m = i;
+                    int n = i + 1;
+                    while (m >= 0 && n < s.length()) {
+                        if (s.charAt(m) == s.charAt(n)) {
+                            palindrome.addFirst(s.charAt(m));
+                        } else {
+                            break;
+                        }
+                        m--;
+                        n++;
+                    }
+                    int k = palindrome.size() - 1;
+                    while (k >= 0) {
+                        palindrome.add(palindrome.get(k));
+                        k--;
+                    }
+                } else {
+                    int m = i - 1;
+                    int n = i + 1;
+                    if (m >= 0 && n < s.length() &&
+                            s.charAt(m) == s.charAt(n)) {
+                        palindrome.addFirst(s.charAt(i));
+                        palindrome.addFirst(s.charAt(m));
+                        m--;
+                        n++;
+                        while (m >= 0 && n < s.length()) {
+                            if (s.charAt(m) == s.charAt(n)) {
+                                palindrome.addFirst(s.charAt(m));
+                            } else {
+                                break;
+                            }
+                            m--;
+                            n++;
+                        }
+                        int k = palindrome.size() - 2;
+                        while (k >= 0) {
+                            palindrome.add(palindrome.get(k));
+                            k--;
+                        }
+                    }
                 }
             }
-            stack.push(s.charAt(i));
+            if (palindrome.size() > palindromeMax.size()) {
+                palindromeMax.clear();
+                palindromeMax.addAll(palindrome);
+            }
             i++;
         }
-        while (i < s.length()) {
+        StringBuilder result = new StringBuilder();
+        i = 0;
+        while (i < palindromeMax.size()) {
+            result.append(palindromeMax.get(i));
+            i++;
+        }
+        return result.toString();
+    }
 
+    // Manacher算法 0(n)
+    public static String longestPalindrome2(String s) {
+        if (s == null || s.isEmpty()) {
+            return "";
         }
         return "";
     }
 
     public static void main(String[] args) {
-        longestPalindrome("babad");
+        String result = longestPalindrome1("ccc");
+        System.out.println(result);
     }
 }
